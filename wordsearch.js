94051e47c016887ad06c95d0d18f21cb7b8d9bca
Sam Roy-Effer <samroyeffer@gmail.com>
2021-06-02 15:41:35 -0400

    fixed and Working file

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,23 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map((ls) => ls.join(""));
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+      if (l.includes(word)) return true;
     }
-}
-
-module.exports = wordSearch
+  
+    let array = [];
+    for (let i = 0; i < letters[0].length; i++) {
+      array.push([]);
+    }
+  
+    for (let row = 0; row < letters.length; row++) {
+      for (let col = 0; col < letters[row].length; col++) {
+        array[col][row] = letters[row][col];
+      }
+      const connectedWord = array.map((ls) => ls.join(""));
+      if (connectedWord.includes(word)) return true;
+    }
+    return false;
+  };
+  
+  module.exports = wordSearch;
+  
